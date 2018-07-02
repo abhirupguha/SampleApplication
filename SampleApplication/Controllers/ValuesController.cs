9b253c34c727421dc2f79bf360266921eb224453
Abhirup Guha <abhirup.guha94@gmail.com>
2018-07-02 09:53:13 +0000

    first commit

---
+++ SampleApplication/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ValuesController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SampleApplication.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
